9a8865e6d626bb78463b9d2be8cb0d449beb1600
JoonHyeok Shin <sj124h@naver.com>
2025-09-23 12:09:38 +0900

    Firebase Initialize

---
+++ twitter-clone/src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "APIKEY",
+  authDomain: "AUTHDOMAIN",
+  projectId: "PROJECTID",
+  storageBucket: "STORAGEBUCKET",
+  messagingSenderId: "MESSAGING",
+  appId: "APPID",
+  measurementId: "MEASUREMENT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
