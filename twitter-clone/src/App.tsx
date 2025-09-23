86bae140e2b739d869184f981470e7ac46c7ddc2
JoonHyeok Shin <sj124h@naver.com>
2025-09-23 11:43:15 +0900

    react : 기본 라우터 설정

--- twitter-clone/src/App.tsx
+++ twitter-clone/src/App.tsx
@@ -1,6 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Layout from "./components/layout.tsx"
+import Home from "./routes/home.tsx"
+import Profile from "./routes/profile.tsx"
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <Layout />,
+    children : [
+      {
+        path: "",
+        element: <Home />,
+        
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      }
+    ]
+
+  }
+
+
+])
+
 function App() {
   return (
-    <> </>
+    <> 
+      <RouterProvider router = {router} />
+    </>
   )
 }
 
